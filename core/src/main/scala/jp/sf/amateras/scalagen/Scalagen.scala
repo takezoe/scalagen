18957c638a18650b82ad2e61c3a9ce9bd4c03250
takezoe <takezoe@gmail.com>
2012-06-23 03:48:57 +0900

    Change the method name from runWith() to generateFor().

--- core/src/main/scala/jp/sf/amateras/scalagen/Scalagen.scala
+++ core/src/main/scala/jp/sf/amateras/scalagen/Scalagen.scala
@@ -5,7 +5,7 @@ package jp.sf.amateras.scalagen
  */
 object Scalagen {
 
-  def runWith[T <: Generator](settings: Settings)(implicit m: scala.reflect.Manifest[T]): Unit = {
+  def generateFor[T <: Generator](settings: Settings)(implicit m: scala.reflect.Manifest[T]): Unit = {
     val c = m.erasure.getConstructor(classOf[Settings])
     c.newInstance(settings) match {
       case generator: Generator => generator.generate(new SchemaLoader(settings).loadSchema())
