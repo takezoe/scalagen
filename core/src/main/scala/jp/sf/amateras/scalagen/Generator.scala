9aa4d50eb9f1ffff2ad0ca5e4335486a3fe1b316
takezoe <takezoe@gmail.com>
2012-06-23 02:01:27 +0900

    Add GeneratorBase which is an utility for implementing Generator.

--- core/src/main/scala/jp/sf/amateras/scalagen/Generator.scala
+++ core/src/main/scala/jp/sf/amateras/scalagen/Generator.scala
@@ -1,5 +1,8 @@
 package jp.sf.amateras.scalagen
 
+import java.io.File
+import jp.sf.amateras.scala.util.io._
+
 /**
  * A trait for source code generators.
  */
@@ -9,5 +12,28 @@ trait Generator {
    * Generates source code.
    */
   def generate(tables: List[Table]): Unit
+
+}
+
+/**
+ * A base class for source code generators.
+ */
+abstract class GeneratorBase(targetDir: File, charset: String = "UTF-8") extends Generator {
+
+  def generate(tables: List[Table]): Unit = {
+    tables.foreach { table =>
+      val generateFileInfo = generate(table)
+      val file = new File(generateFileInfo.outputDir, generateFileInfo.filename)
+      file.write(generateFileInfo.content, charset)
+    }
+  }
+
+  /**
+   * Returns information of the source file for the given table model.
+   * Implement this method at the subclass.
+   */
+  def generate(table: Table): GenerateFileInfo
+
+  case class GenerateFileInfo(outputDir: File, filename: String, content: String)
 
 }
