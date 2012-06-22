2410c4c3e1f2de4f52b80c8473d2f436435c99eb
takezoe <takezoe@gmail.com>
2012-06-23 01:58:59 +0900

    Add Scaladoc.

--- core/src/main/scala/jp/sf/amateras/scalagen/Models.scala
+++ core/src/main/scala/jp/sf/amateras/scalagen/Models.scala
@@ -1,5 +1,18 @@
 package jp.sf.amateras.scalagen
 
+/**
+ * The table model.
+ *
+ * @param name the table name
+ * @param columns the list of column
+ */
 case class Table(name: String, columns: List[Column])
 
+/**
+ * The column model.
+ *
+ * @param name the column name
+ * @param dataType the date type of the column
+ * @param nullable the nullable flag
+ */
 case class Column(name: String, dataType: Class[_], nullable: Boolean)
