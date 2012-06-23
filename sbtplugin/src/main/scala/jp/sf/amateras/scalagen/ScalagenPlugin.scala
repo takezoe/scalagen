c87d6c5509af854d81aa1be90d31a061d79742f9
takezoe <takezoe@gmail.com>
2012-06-23 18:53:39 +0900

    task name is changed to "scalagen" from "generate".

--- sbtplugin/src/main/scala/jp/sf/amateras/scalagen/ScalagenPlugin.scala
+++ sbtplugin/src/main/scala/jp/sf/amateras/scalagen/ScalagenPlugin.scala
@@ -7,7 +7,7 @@ import sbt.Defaults._
 import sbt.Package.ManifestAttributes
 
 object ScalagenPlugin extends Plugin {
-  val scalagen = TaskKey[Unit]("generate", "Generates source code for ORMs")
+  val scalagen = TaskKey[Unit]("scalagen", "Generates source code for ORMs")
 
   val scalagenConfiguration = SettingKey[Settings]("scalagen-settings")
 
