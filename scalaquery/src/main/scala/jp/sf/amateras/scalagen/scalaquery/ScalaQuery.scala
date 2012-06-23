fbb135391d0a2543a97f37329c5a0fe2a69a4bd6
takezoe <takezoe@gmail.com>
2012-06-23 10:18:33 +0900

    Rename ScalaQueryGenerator to ScalaQuery.

--- scalaquery/src/main/scala/jp/sf/amateras/scalagen/scalaquery/ScalaQuery.scala
+++ scalaquery/src/main/scala/jp/sf/amateras/scalagen/scalaquery/ScalaQuery.scala
@@ -2,7 +2,7 @@ package jp.sf.amateras.scalagen.scalaquery
 
 import jp.sf.amateras.scalagen._
 
-class ScalaQueryGenerator(settings: Settings) extends GeneratorBase(settings) {
+class ScalaQuery(settings: Settings) extends GeneratorBase(settings) {
 
   lazy val templatePath = "/jp/sf/amateras/scalagen/scalaquery/scalaquery.ssp"
 
