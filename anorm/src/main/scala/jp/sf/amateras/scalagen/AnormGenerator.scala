90500ae2049a8d998873b4c435529e8dcfb4a1e9
takezoe <takezoe@gmail.com>
2012-07-29 13:21:04 +0900

    #3 AnormGenerator was moved to ScalateGeneratorBase.

--- anorm/src/main/scala/jp/sf/amateras/scalagen/AnormGenerator.scala
+++ anorm/src/main/scala/jp/sf/amateras/scalagen/AnormGenerator.scala
@@ -2,43 +2,25 @@ package jp.sf.amateras.scalagen
 
 import jp.sf.amateras.scalagen._
 
+object AnormGenerator extends App {
+  /**
+   * Adds Column#properyType() to use in the SSP template.
+   */
+  implicit def extendsColumn(column: Column) = new {
+    def propertyType(): String =
+      if(column.primaryKey){
+        "Pk[" + column.dataType + "]"
+      } else if(column.nullable) {
+        "Option[" + column.dataType + "]"
+      } else {
+        column.dataType
+      }
+  }
+}
+
 /**
  * A generator implementation for Anorm.
  */
-class AnormGenerator extends GeneratorBase {
-
-  def generate(settings: Settings, table: Table): String = {
-    import settings._
-
-    (if(packageName == ""){
-      ""
-    } else {
-      "package " + packageName + "\n\n"
-    }) +
-    "import import anorm._\n" +
-    "import anorm.SqlParser._\n\n" +
-    "case class " + table.className + "(\n" +
-    table.columns.map { column => "  " + column.propertyName + ": " + propertyType(column) }.mkString(",\n") +
-    "\n" +
-    ")\n\n"
-    "object " + table.className + "{\n" +
-    "  val simple = {" +
-    table.columns.map { column =>
-    "    get[" + propertyType(column) + "](\"" + column.name + "\"),"
-    }.mkString("\n") + "map {\n" +
-    "      case " + table.columns.map(_.propertyName).mkString(" ~ ") + " => " + table.className + "(" + table.columns.map(_.propertyName).mkString(", ") + ")\n" +
-    "    }\n" +
-    "  }\n" +
-    "}\n\n"
-  }
-
-  private def propertyType(column: Column): String =
-    if(column.primaryKey){
-      "Pk[" + column.dataType + "]"
-    } else if(column.nullable) {
-      "Option[" + column.dataType + "]"
-    } else {
-      column.dataType
-    }
-
+class AnormGenerator extends ScalateGeneratorBase {
+  val templatePath = "/jp/sf/amateras/scalagen/AnormGenerator.ssp"
 }
